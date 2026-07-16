99a152fe1b204dd89cfb64be05c8cd06af190994
coperlm <coperlm@users.noreply.github.com>
2026-07-16 13:25:31 +0000

    deploy: 6ef6e7a68cfd65619e767256829a701ea2d35484

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6f4e895a.js',
+                path_to_root + 'searchindex-532edccc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
